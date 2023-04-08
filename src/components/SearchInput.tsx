ba76c20bb7e7ec7543645e67355912a931aab8fe
Nnadi Victory <chikamagozi25@gmail.com>
2023-04-08 22:33:09 +0100

    Change input type to number instead of tel

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -15,7 +15,7 @@ const SearchInput = ({ onSubmit }: Props) => {
     }
     return (
         <form action="#" onSubmit={(e) => handleSubmit(e)} className="w-[90%] mx-auto max-w-xl mt-7 flex items-center">
-            <input ref={inputRef} required minLength={11} type="tel" className="bg-white rounded-l-xl text-lg h-14 px-4 w-full outline-none" placeholder="Search for any IP address" />
+            <input ref={inputRef} required minLength={11} type="number" className="bg-white rounded-l-xl text-lg h-14 px-4 w-full outline-none" placeholder="Search for any IP address" />
             <button className='bg-black h-14 p-5 rounded-r-xl hover:bg-vDarkGrey' type="submit"><AiOutlineRight color='#fff' /></button>
         </form>
     );
